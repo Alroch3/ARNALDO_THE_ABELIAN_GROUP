fa0d6d707a0d77c0f7ebd65555279d231ab3b456
Alroch3 <roberto.mancon@gmail.com>
2024-04-28 11:53:58 +0200

    aggiunta scelta utente

--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Persona.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Persona.java
@@ -7,6 +7,17 @@ public class Persona {
     private String luogoDiNascita;
     private String cF;
     private String scadenza;
+    private boolean sceltaUtente;       //scelta di ingresso o meno della persona da parte dell'utente ( non quella giusta )
 
+    public Persona(String nome, String cognome, String sesso, String data, String luogoDiNascita, String cF,
+            String scadenza) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.sesso = sesso;
+        this.data = data;
+        this.luogoDiNascita = luogoDiNascita;
+        this.cF = cF;
+        this.scadenza = scadenza;
+    }
     
 }
