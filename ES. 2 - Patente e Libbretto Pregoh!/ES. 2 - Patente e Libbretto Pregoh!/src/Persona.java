732ecc810ec41e9f5ac5a67caf261167a5f1539f
Alroch3 <roberto.mancon@gmail.com>
2024-04-28 11:37:11 +0200

    primo commit - aggiunta XML e classe persona

--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Persona.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Persona.java
@@ -1,3 +1,12 @@
 public class Persona {
+
+    private String nome;
+    private String cognome;
+    private String sesso;
+    private String data;
+    private String luogoDiNascita;
+    private String cF;
+    private String scadenza;
+
     
 }
