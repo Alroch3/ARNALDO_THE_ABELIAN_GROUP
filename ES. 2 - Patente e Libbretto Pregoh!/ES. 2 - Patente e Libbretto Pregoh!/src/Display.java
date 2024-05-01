763dabb46c3ab2be39ba67f69bc14f6ffd0df9c7
Alroch3 <roberto.mancon@gmail.com>
2024-05-01 21:04:52 +0200

    Modifiche in devRoberto, aggiunta libreria kibo e utilizzo per interazioni con l'utente

--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Display.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Display.java
@@ -1,104 +1,91 @@
-import java.util.Scanner;
+import it.kibo.fp.lib.Menu;
 
 public class Display {
-//--------------------------------------------------------------------------------------------------------------
 
-public static void mostraRegolamento(){
+    //---------- REFACTOR ----------//
 
-    //metodo che aiuta l'utente a capire in cosa consiste il gioco e come vincere
- System.out.println("Benvenuto alla frontiera del Burmini! Per non essere licenziato al primo giorno ci sono delle regole che bisogna seguire..."
- + "\n 1. Ogni giorno dovrà controllare delle persone e decidere se possono oltrepassare la frontiera"
- + "\n 2. Se fa passare una persona con passaporto falso/scaduto avrete una possibilià di essere scoperti dal capo e prendere una multa di 300 sbleuri"
- + "\n 3. Se non fa passare una persona che poteva entrare in modo regolare potrete essere puniti e ricevere una multa di 150 sbleuri"
- + "\n 4. Se svolge il suo lavoro e rifiuta una persona non in regola essa potrebbe provare a corromperla...Ci PENSI DUE VOLTE AD ACCETTARE!!!"
- + "\n 5. c'è una buona notizia e una cattiva... ogni persona controllata sono 50 sbleuri ma ogni 3 giorni dovrà pagare 200 sbleuri di tasse...( fossi in lei non evaderei)"
- + "\n (Detto tra noi... il Burmini sta diventando un paese pericoloso, se vuole sopravvivere dovrà intascare 2300 sblueri in 12 giorni e io mi occuperò della sua fuga)");
-    
-}
+    private static final String TITOLO_MENU = "Menu principale";
 
-//--------------------------------------------------------------------------------------------------------------
+    //---------- VOCI MENU ----------//
+    private static final String vociMenu [] = {
+        "ACCETTARE ENTRATA",
+        "RIFIUTARE ENTRATA",
+        "REGOLAMENTO",
+        "COMUNI",
+    };
 
-public static void mostraGiorno(){
+    public static void mostraRegolamento() {             // metodo che aiuta l'utente a capire in cosa consiste il gioco e come vincere
 
-    //metodo che mostra la data odierna 
-}
+        System.out.println("Benvenuto alla frontiera del Burmini! Per non essere licenziato al primo giorno ci sono delle regole che bisogna seguire..."
+                        + "\n 1. Ogni giorno dovrai controllare delle persone e decidere se possono oltrepassare la frontiera"
+                        + "\n 2. Se fai passare una persona con passaporto falso/scaduto avrai una possibilià di essere scoperti dal capo e prendere una multa di 300 sbleuri"
+                        + "\n 3. Se non fai passare una persona che poteva entrare in modo regolare potrai essere punito e ricevere una multa di 150 sbleuri"
+                        + "\n 4. Se svolgi il suo lavoro e rifiuta una persona non in regola essa potrebbe provare a corromperla...Ci PENSI DUE VOLTE AD ACCETTARE!!!"
+                        + "\n 5. c'è una buona notizia e una cattiva... ogni persona controllata sono 50 sbleuri ma ogni 3 giorni dovrà pagare 200 sbleuri di tasse...( fossi in lei non evaderei)"
+                        + "\n (Detto tra noi... il Burmini sta diventando un paese pericoloso, se vuole sopravvivere dovrà intascare 2300 sblueri in 12 giorni e io mi occuperò della sua fuga)");
 
-//--------------------------------------------------------------------------------------------------------------
-public static void mostraComuni(){
+    }
 
+    public static void mostraGiorno() {
 
-    //per mostrare l'elenco dei comuni validi
+        // metodo che mostra la data odierna
+    }
 
-}
+    public static void mostraComuni() {
 
-//--------------------------------------------------------------------------------------------------------------
+        // per mostrare l'elenco dei comuni validi
 
-public static void visualizzaPersona(){
+    }
 
-    //per visulaizzare i dati della persona da esaminare a schermo
-}
+    public static void visualizzaPersona() {
 
-//--------------------------------------------------------------------------------------------------------------
+        // per visulaizzare i dati della persona da esaminare a schermo
+    }
 
-public static void mostraContoResiduo(){
+    public static void mostraContoResiduo() {
 
-    //per visualizzare il conto residuo
-}
+        // per visualizzare il conto residuo
+    }
 
-//--------------------------------------------------------------------------------------------------------------
+    public static void mostraSpese() { // mostra le spese dovute hai giorni precedenti per vari motivi
+        // si può anche non fare
+    }
 
-public static void mostraSpese(){ // mostra le spese dovute hai giorni precedenti per vari motivi
-//si può anche non fare
-}
+    public static void mostraGiorniRimanenti() {
 
-//--------------------------------------------------------------------------------------------------------------
+        // mostra quanti giorni restano alla fine
+    }
 
-public static void mostraGiorniRimanenti(){
+    public static int visualizzaMenu() {// da richiamare su richiesta dell'utente
 
-    //mostra quanti giorni restano alla fine
-}
+        Menu menuPrincipale = new Menu(TITOLO_MENU, vociMenu, true, true, true);
+        int scelta = menuPrincipale.choose();
+        return scelta;
 
-//--------------------------------------------------------------------------------------------------------------
+    }
 
+    public static void visualizzaInterfacciaIniziale() {// da richiamare solo la prima volta all'inizio
 
-
-public static void visualizzaMenu(){//da richiamare su richiesta dell'utente
-
-System.out.println("\n opzione 1 : REGOLAMENTO");
-System.out.println("\n opzione 2 : GIORNO");
-System.out.println("\n opzione 3 : COMUNI");
-System.out.println("\n opzione 4 : CONTO RESIDUO");
-System.out.println("\n opzione 5 : GIORNI RIMANENTI");
-System.out.println("\n opzione 6 : Prosegui");
-System.out.print("Scriva il numero dell'opzione che desidera visualizzare: ");
-  
-}
-
-//--------------------------------------------------------------------------------------------------------------
-
-public static void visualizzaInterfacciaIniziale(){// da richiamare solo la prima volta all'inizio
-
-    System.out.println("“Congratulazioni!\r\n" + 
-                "La lotteria del lavoro di aprile è terminata. È stato estratto il tuo nome, Ajeje Brazorf. Fai rapporto al Ministero \r\n" + //
-                "dell'Accoglienza presso il presidio di frontiera Muskalf per il collocamento immediato. Alla tua famiglia verrà \r\n" + //
-                "assegnato un appartamento di classe 8 a Muskalf Est.\r\n" + 
+        System.out.println("“Congratulazioni!\r\n" +
+                "La lotteria del lavoro di aprile è terminata. È stato estratto il tuo nome, Ajeje Brazorf. Fai rapporto al Ministero \r\n"
+                + //
+                "dell'Accoglienza presso il presidio di frontiera Muskalf per il collocamento immediato. Alla tua famiglia verrà \r\n"
+                + //
+                "assegnato un appartamento di classe 8 a Muskalf Est.\r\n" +
                 "BUON LAVORO E GLORIA AL BURMINI!” ");
+    }
+    /*
 
-                visualizzaMenu();
+    public static void mostraInterfacciaScelta() { // viene richiamata dal metodo fai scelta e mostra il giorno, spese,
+                                                   // conto e la persona da accttare o rifiuare
 
-}
+        mostraGiorno();
+        System.out.println("\n\n\n");
+        mostraSpese();
+        mostraContoResiduo();
+        System.out.println("\n\n\n");
+        visualizzaPersona();
 
-//--------------------------------------------------------------------------------------------------------------
-
-public static void mostraInterfacciaScelta(){ //viene richiamata dal metodo fai scelta e mostra il giorno, spese, conto  e la persona da accttare o rifiuare
-
-    mostraGiorno();
-    System.out.println("\n\n\n");
-    mostraSpese();
-    mostraContoResiduo();
-    System.out.println("\n\n\n");
-    visualizzaPersona();
-
-
-}
+    }
+    */
 }
--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/InterazioniUtente.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/InterazioniUtente.java
@@ -1,95 +1,35 @@
-import java.util.Scanner;
-
 public class InterazioniUtente {
 
-private Scanner scanner = new Scanner(System.in);
+  private static final String MSG_ENTRATA_ACCETTATA = "ENTRATA ACCETTATA";
+  private static final String MSG_ENTRATA_RIFIUTATA = "ENTRATA RIFIUTATA";
 
-//----------------------------------------------------------------------------------------------------------------------------
-public void erroreDiScelta(){ 
+  public static void sceltaMenu() {
 
-    System.out.println("numero di opzione non valido, riprova");
-
-}
-
-//-------------------------------------------------------------------------------------------------------------------------------
-
-public void faiScelta(){ // metodo che mostra l'interfaccia della scelta, permette all'utente di scegliere se far entrare o no la persona e di aprire il menu
-
-   Display.mostraInterfacciaScelta();
-   System.out.println("\n 0.RIFIUTARE  1.ACCETTARE  2.Menu");
-   System.out.print("scelta: ");
-   int scelta = scanner.nextInt();
-   
-  switch(scelta){
-    
-    case 0:  
-        System.out.println("entrata rifiutata");
-        seRifiuta(); //metodo dalla classe controllore (da creare)
-
-    break;
-
-    case 1 :
-         System.out.println("entrata accettata");
-        seAccetta(); //metodo dalla classe controllore (dac creare)
-    break;
-
-    case 2 :
-        sceltaMenu();
-    break;
-
-    default:
-      erroreDiScelta();
-      faiScelta();
-  }
-  
-}
-
-//--------------------------------------------------------------------------------------------------------------------------------
-
-
-  public void sceltaMenu(){// metodo che permette di scegliere un opzione del menu
- 
-    Display.visualizzaMenu();
-   int opzione = scanner.nextInt();
-
-   switch(opzione){
-
-    case 1:
-    Display.mostraRegolamento();
-    break;
-
-    case 2:
-    Display.mostraGiorno();
-    break;
-
-
-    case 3:
-    Display.mostraComuni();
-    break;
-
-
-    case 4:
-    Display.mostraContoResiduo();
-    break;
-
-
-    case 5:
-    Display.mostraGiorniRimanenti();
-    break;
-
-
-    case 6:
-    faiScelta();// nel cliccare prosegui si ritorna a decidere se approvare o rifiutare la persona 
-    break;
-
-    default:
-    erroreDiScelta();
-    sceltaMenu();
-
-   }
-
+    int opzione = Display.visualizzaMenu();
+    switch (opzione) {
+      case 1:                             //Entrata accettata
+        seAccetta();
+        break;
+      case 2:                             //Entrata rifiutata
+        seRifiuta();                      
+        break;
+      case 3:                             //Scelta di mostrare il regolamento
+        Display.mostraRegolamento();      
+        break;
+      case 4:
+        Display.mostraComuni();           //Scelta di mostrare l'elenco dei comuni
+        break;
+      case 0:
+        //gestire l'uscita dal gioco
+        break;
+    }
   }
 
-//-----------------------------------------------------------------------------------------------------------------------------------
+  private static void seRifiuta() {
+    System.out.println(MSG_ENTRATA_RIFIUTATA);
+  }
 
+  private static void seAccetta() {
+    System.out.println(MSG_ENTRATA_ACCETTATA);
+  }
 }
