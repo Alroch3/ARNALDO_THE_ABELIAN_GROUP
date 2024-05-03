5e67cb1b4af3256b64ea8c1547f2025d3b465db1
Alroch3 <roberto.mancon@gmail.com>
2024-05-03 23:14:20 +0200

    Implementazione metodo controlla persona in Controllore - VERIFICARE FUNZIONAMENTO

--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Controllore.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Controllore.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Controllore {
 
   private static boolean giudizioScelta= false;
@@ -28,7 +30,7 @@ public static boolean seRifiuta(Persona personaRifutata){ //il metodo controlla 
 
    boolean risultato = controllaDatiPersona(personaRifutata);
     
-   if(risultato != false  ){ //ovvero se i dati sono sbagliati e l'utente ha rifiutati allora ritornerà true altrimenti false 
+   if(risultato == false  ){ //ovvero se i dati sono sbagliati e l'utente ha rifiutati allora ritornerà true altrimenti false 
 
     giudizioScelta = true;
 
@@ -42,18 +44,50 @@ public static boolean seRifiuta(Persona personaRifutata){ //il metodo controlla 
 
   }
 
-public static boolean controlloCF(){
-  
+public static boolean controllaDatiPersona(Persona p, LocalDate giornoCorrente){      //metodo che controlla se il codice fiscale della persona è valido
+  if(posizioniCorretteCF(p.getcF()) && (p.getSesso().equals(validitaGiornoESesso(p.getcF()))) && meseValido(p.getcF(), giornoCorrente)){
+    return true;
+  }
+  return false;
 }
 
 
-public static boolean controllaDatiPersona(Persona personaDaControllare){ // il metodo controlla i dati della persona se sono giusti o sbagliati
 
+private static String validitaGiornoESesso(String CF) {                           //questo metodo fa 2 controlli, il primo è se il giorno di nascita è valido e il secondo se è maschio o femmina perchè nel CF le donne hanno il numero di nascita aumentato di 40
+  int giorno = Integer.parseInt(CF.substring(9, 11));       //qui trasformo in un intero il numero del giorno che viene estrapolato con CF.substring
+  if(giorno >= 1 && giorno <=31){
+    return "M";
+  }
+  if(giorno >= 41 && giorno <= 71){
+    return "F";
+  }
+  return "I";
 
-  //ANCORA DA IMPLEMENTARE 
+}
 
-  return risultatoControllo;//TRUE SE I DATI SONO CORRETTI , FALSE SE SONO SBAGLIATI
+private static boolean posizioniCorretteCF(String CF) {
+  for(int i=0; i<16; i++){
+    if((i>=0 && i<=5) || i==8 || i==15 || i==11){
+      if(!Character.isLetter(CF.charAt(i))){
+        return false;
+      }
+    }else{
+      if(!Character.isDigit(CF.charAt(i))){
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
+private static boolean meseValido(String CF, LocalDate giornoCorrente){
+  final String lettereConsentitePerMese = "ABCDEHLMPRST";                               //questo attributo specifica quali lettere sono consentite nel mese del CF
+  if(lettereConsentitePerMese.contains(Character.toString(CF.charAt(8)))){        //questo verifica se il carattere del mese, all'interno del codice fiscale è uno dei caratteri possibili      
+    if(Character.toString(CF.charAt(8)).equals(Character.toString(lettereConsentitePerMese.charAt(giornoCorrente.getMonthValue())))){
+      return true;
+    }
+  }
+  return false;
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Main.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import it.kibo.fp.lib.InputData;
 
 public class Main {
@@ -9,8 +12,18 @@ public class Main {
             Display.mostraRegolamento();
         }
         
-        System.out.println(GestioneXML.cercaComuneDaXML("YUWHC"));
+        System.out.println(GestioneXML.cercaComuneDaXML("SANGANO"));
 
+        LocalDate dataPersonalizzata = LocalDate.of(2023, 5, 15);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
+        String dataFormattata = dataPersonalizzata.format(formatter);
+        System.out.println(dataFormattata);
+
+        
+        Persona p1 = new Persona("", "", "", "", "", "", "");
+        GestioneXML.creaPersonaDaXML(p1, "5");
+        System.out.println(Controllore.controllaDatiPersona(p1, dataPersonalizzata));
         while(true){                                                            //implementare le condizioni affinchè si ha la fine del gioco
             GiornoLavorativo giorno = new GiornoLavorativo();
             //crea un oggetto GiornoLavorativo
