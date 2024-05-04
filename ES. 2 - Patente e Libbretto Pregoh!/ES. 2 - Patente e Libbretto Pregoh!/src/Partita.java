60169d2e107893eeed59be6587351650e1dcbcbe
Alroch3 <roberto.mancon@gmail.com>
2024-05-04 18:14:27 +0200

    Creazione metodo gioco in Partita, e sistemazione classe GiornoLavorativo

--- ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Partita.java
+++ ES. 2 - Patente e Libbretto Pregoh!/ES. 2 - Patente e Libbretto Pregoh!/src/Partita.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.*;
 
 import it.kibo.fp.lib.*;
@@ -6,26 +7,34 @@ import it.kibo.fp.lib.*;
 public class Partita {
 
     //creo un Array di giorni lavorativi
-    private GiornoLavorativo [] giorniPartita= new GiornoLavorativo[11];
-    
-    public Partita(GiornoLavorativo [] giorniPartita){
-        giorniPartita= this.giorniPartita;
-    }
+    private static GiornoLavorativo [] giorniPartita = new GiornoLavorativo[11];
+    private static LocalDate dataIniziale = LocalDate.of(2024, 4, 26)
 
     //metodo da richiamare nel main per far andare il gioco!
-    
-    
     public static void gioco(){
+        Display.visualizzaInterfacciaIniziale();
+        if(InputData.readYesOrNo("Vuoi visualizzare il regolamento")){
+            Display.mostraRegolamento();
+        }
+        ;
+
         //do while che segna lo scandire dei giorni
         do{
-        int posizioneArray=0;
-        GiornoLavorativo nuovoGiorno= new GiornoLavorativo(aggiungiUnGiorno(), creaArrayDiPersone());
-        giorniPartita[i]=nuovoGiorno;
-        posizioneArray++;   
+            int posizioneArray = 0;
+            creaGiorno(posizioneArray);
+            
+            posizioneArray++;  
 
         }while(Finali.farProseguireIlGioco());
         //sistemare la questione package, non posso fare import java.nomeclasse!!
     }
+
+    public static void creaGiorno(int indice){
+        GiornoLavorativo nuovoGiorno = new GiornoLavorativo(dataIniziale.plusDays(indice));
+        nuovoGiorno.creaArrayDiPersone();
+        giorniPartita[indice] = nuovoGiorno;
+    }
+}
 
 
 
